eedb987322cd5a53346133c44a586b22b1c751e7
arajajyothibabu <arajajyothibabu@gmail.com>
2016-03-19 01:55:11 +0530

    membership functions about to start

--- src/controllers/membership_functions/trapezoidal/TrapezoidalInput.java
+++ src/controllers/membership_functions/trapezoidal/TrapezoidalInput.java
@@ -5,9 +5,6 @@ package controllers.membership_functions.trapezoidal;
  */
 public class TrapezoidalInput {
 
-    //TODO: Write input and output functions you are about to write these functions by tmrw afternoon //okay like comment
-    // these are changes
-    //like this you need to crate ne branch for very operation or special module you are working on
-    // and special commit for every unit of work u do
-    //now let commit this comments as of now. tmrw u write actual <code></code>
+    //TODO: Write input and output functions trapezoidal input ki output ki veerega ceate cheyal//names
+    //just like triangular package
 }
--- src/controllers/membership_functions/trapezoidal/TrapezoidalOutput.java
+++ src/controllers/membership_functions/trapezoidal/TrapezoidalOutput.java
@@ -4,5 +4,5 @@ package controllers.membership_functions.trapezoidal;
  * Created by Araja Jyothi Babu on 15-Mar-16.
  */
 public class TrapezoidalOutput {
-    //TODO: Write input and output functions
+    //TODO: Write input and output
 }
