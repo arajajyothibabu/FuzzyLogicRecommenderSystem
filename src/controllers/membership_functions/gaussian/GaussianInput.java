363435f4e6a355189eced17777918cb3a12bb769
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-19 15:37:36 +0530

    sample guassian memebership function

--- src/controllers/membership_functions/gaussian/GaussianInput.java
+++ src/controllers/membership_functions/gaussian/GaussianInput.java
@@ -1,10 +1,50 @@
 package controllers.membership_functions.gaussian;
 
+import utils.Utils;
+
 /**
  * Created by Araja Jyothi Babu on 15-Mar-16.
  */
 public class GaussianInput {
 
-    //TODO: Write input and output functions
+    public static double makeInputToExp(double x, double m, double sigma) {
+        return -(0.5) * Utils.square((x - m) / sigma);
+    }
+
+    public static double exp(double value){
+       return Math.exp(value);
+    }
+    //Average rating by User
+
+    public static double avgRatingByUser_low(double x){
+        return exp(makeInputToExp(x, 0, 0.8493));
+    }
+
+    public static double avgRatingByUser_medium(double x){
+        //TODO:
+        return 0;
+    }
+
+    public static double avgRatingByUser_high(double x){
+        //TODO
+        return 0;
+    }
+
+    //Average rating to movie
+
+    public static double avgRatingToMovie_low(double x){
+        //TODO
+        return 0;
+    }
+
+    public static double avgRatingToMovie_medium(double x){
+        //TODO
+        return 0;
+    }
+
+    public static double avgRatingToMovie_high(double x){
+        //TODO
+        return 0;
+    }
 
 }
