bc41d7e48176b6c4e18651d92a78006f861e4340
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-19 18:02:03 +0530

    first Euclidian function metric added

--- src/controllers/distance_metrics/Euclidean.java
+++ src/controllers/distance_metrics/Euclidean.java
@@ -1,14 +1,27 @@
 package controllers.distance_metrics;
 
+import models.*;
+import utils.DB;
+import utils.Utils;
+
+import java.util.ArrayList;
+
 /**
  * Created by Araja Jyothi Babu on 15-Mar-16.
  */
 public class Euclidean {
 
     //TODO:public static double dissimilarityBetweenUser(ArrayList<>users)
-    public static double dissimilarityBetweenUsers(String userId){
-
-        return 0;
+    //FIXME: userId ot userObject need to fix later
+    public static double dissimilarityBetweenUsers(User userU, User userV) throws Exception{
+        ArrayList<Movie> movieList = DB.getMovies();
+        double sumOfSquares = 0;
+        double ratingDifference = 0;
+        for(Movie movie : movieList){
+            ratingDifference = DB.ratingOfUserToMovie(userU.userId, movie.movieID).rating - DB.ratingOfUserToMovie(userV.userId, movie.movieID).rating;
+            sumOfSquares += Utils.square(ratingDifference);
+        }
+        return Math.sqrt(sumOfSquares);
     }
 
     public static double dissmilarityBetweenMovies(){
