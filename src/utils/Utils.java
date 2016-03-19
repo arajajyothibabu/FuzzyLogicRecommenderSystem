09a464fe02805865d67f685b8ab324e2f9abd8c8
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-19 13:22:38 +0530

    make movie helper function in utils

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -16,9 +16,9 @@ public class Utils {
         return new User(user.getInt(1), user.getString(2).charAt(0), user.getInt(3), user.getString(4), user.getInt(5));
     }
 
-    /*public static Movie makeMovie(ResultSet movie) throws Exception {
-        return new Movie(movie.getInt(1), user.getString(2).charAt(0), user.getInt(3), user.getString(4), user.getInt(5));
-    }*/
+    public static Movie makeMovie(ResultSet movie) throws Exception {
+        return new Movie(movie.getInt(1), movie.getString(2), movie.getString(3));
+    }
 
     public static Rating makeRating(ResultSet rating) throws Exception {
         return new Rating(rating.getInt(1), rating.getInt(2), rating.getInt(3), rating.getString(4));
