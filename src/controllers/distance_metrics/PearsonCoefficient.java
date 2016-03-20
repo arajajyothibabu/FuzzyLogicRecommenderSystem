26229937930dab1eaf67cf66717eb0a2e8cc4b69
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-20 16:51:33 +0530

    pearson dissimilarity metric added

--- src/controllers/distance_metrics/PearsonCoefficient.java
+++ src/controllers/distance_metrics/PearsonCoefficient.java
@@ -1,8 +1,8 @@
 package controllers.distance_metrics;
 
 import controllers.OracleDAO;
-import models.Rating;
-import models.User;
+import models.*;
+import utils.Utils;
 
 import java.util.ArrayList;
 
@@ -20,6 +20,28 @@ public class PearsonCoefficient {
         int actualNumberOfRatingsByUser = ratingsCount > 0? ratingsCount : 1;
         double averageRating = totalRating / actualNumberOfRatingsByUser;
         return averageRating;
+    }
+
+    public static double dissimilarityBetweenUsers(User userU, User userV) throws Exception{
+        ArrayList<Movie> movieList = OracleDAO.getMovies();
+        double sumOfDifferencesOfUserU = 0, sumOfDifferencesOfUserV = 0, numeratorSum = 0, denominatorSumUserU = 0, denominatorSumUserV = 0, userUDifference = 0, userVDifference = 0;
+        double averageRatingOfUserU = averageRatingOfUser(userU);
+        double averageRatingOfUserV = averageRatingOfUser(userV);
+        int ratingOfUserUForSingleMovie = 0, ratingOfUserVForSingleMovie = 0;
+        for(Movie movie : movieList){
+            ratingOfUserUForSingleMovie = OracleDAO.ratingOfUserToMovie(userU.userId, movie.movieID).rating;
+            ratingOfUserVForSingleMovie = OracleDAO.ratingOfUserToMovie(userV.userId, movie.movieID).rating;
+            userUDifference = ratingOfUserUForSingleMovie - averageRatingOfUserU;
+            userVDifference = ratingOfUserVForSingleMovie - averageRatingOfUserV;
+            numeratorSum += userUDifference * userVDifference;
+            denominatorSumUserU += Utils.square(userUDifference);
+            denominatorSumUserV += Utils.square(userVDifference);
+        }
+        return numeratorSum / (Math.sqrt(denominatorSumUserU) * Math.sqrt(denominatorSumUserV));
+    }
+
+    public static double dissmilarityBetweenMovies(){
+        return 0;
     }
 
 }
