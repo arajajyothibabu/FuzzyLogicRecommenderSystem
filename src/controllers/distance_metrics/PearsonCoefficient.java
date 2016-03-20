0874ebbf05c32c9bd49f31261df3e775fb2695d0
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-20 22:43:04 +0530

    movie genre dissimilarity in pearson added

--- src/controllers/distance_metrics/PearsonCoefficient.java
+++ src/controllers/distance_metrics/PearsonCoefficient.java
@@ -17,9 +17,18 @@ public class PearsonCoefficient {
         for(Rating rating : ratingsOfUser)
             totalRating += rating.rating;
         int ratingsCount = ratingsOfUser.size();
-        int actualNumberOfRatingsByUser = ratingsCount > 0? ratingsCount : 1;
+        int actualNumberOfRatingsByUser = ratingsCount > 0 ? ratingsCount : 1;
         double averageRating = totalRating / actualNumberOfRatingsByUser;
         return averageRating;
+    }
+
+    public static double averageGenreOfMovie(Movie movie) throws Exception {
+        double totalGenre = 0;
+        ArrayList<Integer> genresOfMovie = Utils.makeGenereList(OracleDAO.getGenres(movie.movieID));
+        for(Integer genre : genresOfMovie)
+            totalGenre += genre.intValue();
+        double averageGenre = totalGenre / genresOfMovie.size();
+        return averageGenre;
     }
 
     public static double dissimilarityBetweenUsers(User userU, User userV) throws Exception{
@@ -40,8 +49,20 @@ public class PearsonCoefficient {
         return numeratorSum / (Math.sqrt(denominatorUserUSum) * Math.sqrt(denominatorUserVSum));
     }
 
-    public static double dissmilarityBetweenMovies(){
-        return 0;
+    public static double dissimilarityBetweenMovies(Movie movie1, Movie movie2) throws Exception {
+        ArrayList<String> genreList = Genre.getGenreList();
+        int noOfGenres = genreList.size();
+        double numeratorSum = 0, denominatorMovie1GenreSum = 0, denominatorMovie2GenreSum = 0, movie1Difference = 0, movie2Difference = 0;
+        double averageGenreOfMovie1 = averageGenreOfMovie(movie1);
+        double averageGenreOfMovie2 = averageGenreOfMovie(movie2);
+        for(int i = 0; i < noOfGenres; i++){
+            movie1Difference = movie1.genres.get(i) - averageGenreOfMovie1;
+            movie2Difference = movie2.genres.get(i) - averageGenreOfMovie2;
+            numeratorSum += movie1Difference * movie2Difference;
+            denominatorMovie1GenreSum += Utils.square(movie1Difference);
+            denominatorMovie2GenreSum += Utils.square(movie2Difference);
+        }
+        return numeratorSum / (Math.sqrt(denominatorMovie1GenreSum) * Math.sqrt(denominatorMovie2GenreSum));
     }
 
 }
