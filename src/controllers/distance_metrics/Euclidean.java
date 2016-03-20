fb32fc1dc484def30b4c0ed30871ebfa2b816faa
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-20 18:18:04 +0530

    dissimilarityBetween movies by genres added sample method

--- src/controllers/distance_metrics/Euclidean.java
+++ src/controllers/distance_metrics/Euclidean.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class Euclidean {
 
     //FIXME: userId ot userObject need to fix later
-    public static double dissimilarityBetweenUsers(User userU, User userV) throws Exception{
+    public static double dissimilarityBetweenUsers(User userU, User userV) throws Exception {
         ArrayList<Movie> movieList = OracleDAO.getMovies();
         double sumOfSquares = 0;
         double ratingDifference = 0;
@@ -24,8 +24,15 @@ public class Euclidean {
         return Math.sqrt(sumOfSquares);
     }
 
-    public static double dissmilarityBetweenMovies(Movie movie1, Movie movie2){
-        return 0;
+    public static double dissimilarityBetweenMovies(Movie movie1, Movie movie2) throws Exception {
+        ArrayList<String> genreList = Genre.getGenreList();
+        double sumOfSquares = 0;
+        double genreDifference = 0;
+        for(int i = 0; i < genreList.size(); i++){
+            genreDifference = movie1.genres.get(i) - movie2.genres.get(i);
+            sumOfSquares += Utils.square(genreDifference);
+        }
+        return Math.sqrt(sumOfSquares);
     }
 
 }
