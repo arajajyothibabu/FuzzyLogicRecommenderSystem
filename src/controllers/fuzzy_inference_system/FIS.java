ff582f77b3e6eb8a9f8b2b2ebe811bca1784b49c
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-21 17:41:58 +0530

    FIS module added

--- src/controllers/fuzzy_inference_system/FIS.java
+++ src/controllers/fuzzy_inference_system/FIS.java
@@ -1,7 +1,21 @@
 package controllers.fuzzy_inference_system;
 
+import controllers.OracleDAO;
+import models.User;
+
+import java.util.ArrayList;
+
 /**
  * Created by Araja Jyothi Babu on 21-Mar-16.
  */
 public class FIS {
+
+    //TODO:Average rating based on user choice similarity
+    public static double userChoiceSimilarity(User user, int K) throws Exception {
+        ArrayList<User> otherUsers = OracleDAO.getUsers(user);
+        return 0;
+    }
+
+    //TODO:Average rating based on user acceptance rate
+
 }
